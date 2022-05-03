5cd803791b3975c7933f80d7e03f5ad7075c6369
ensinhu <90516509+kaguya-shinomiya-is-god@users.noreply.github.com>
2022-05-02 21:52:14 -0300

    Add files via upload

---
+++ game/Robos.java
@@ -0,0 +1,25 @@
+package main;
+
+public class Robos{
+
+    //Atributos gerais
+
+    String modelo;
+    int durabilidade;
+    int ataque;
+    boolean vivo = true;
+
+
+    // Metodo para declaração
+    public void status(){
+
+        System.out.println();
+        System.out.println("INFORMAÇÔES DO ROBO:");
+        System.out.println("-------------------");
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Durabilidade: " + durabilidade);
+        System.out.println("Ataque: " + ataque);
+        System.out.println();
+    }
+
+}
